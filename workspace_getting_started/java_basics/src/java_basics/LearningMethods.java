a0a8606e2a086dc4e3089382cd4b294a71831488
Tony D Nguyen <tonydng123@gmail.com>
2023-09-21 12:23:27 -0700

    add LearningMethods class

---
+++ workspace_getting_started/java_basics/src/java_basics/LearningMethods.java
@@ -0,0 +1,13 @@
+package java_basics;
+
+public class LearningMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyUtils.printSomeStuffs();
+		MyUtils.printSomeStuffs("This is an overload method");
+		MyUtils.printSomeStuffs(35);
+	}
+
+	
+}
---
+++ workspace_getting_started/java_basics/src/java_basics/MyUtils.java
@@ -0,0 +1,14 @@
+package java_basics;
+
+public class MyUtils {
+	public static void printSomeStuffs() {
+		System.out.println("Hi there my coders");
+	}
+	public static void printSomeStuffs(String myStr) {
+		System.out.println(myStr);
+	}
+	public static void printSomeStuffs(int num) {
+		System.out.println("Integer overload method of number: " + num);
+	}
+
+}

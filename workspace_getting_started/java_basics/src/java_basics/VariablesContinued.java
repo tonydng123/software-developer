09b09a4ff733ff9bffabea5f3a1af482364e78ca
Tony D Nguyen <tonydng123@gmail.com>
2023-09-21 11:52:58 -0700

    add VariablesContinued class

---
+++ workspace_getting_started/java_basics/src/java_basics/VariablesContinued.java
@@ -0,0 +1,15 @@
+package java_basics;
+
+public class VariablesContinued {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var = -100000000;
+		short bigNumber = -32768;
+		byte reallySmallNumber = -128;
+		double decimalValue = 238.45;
+		boolean decision = true;
+		char letter = 'a';
+	}
+
+}

478ced1a7a66406151101455670c00e5cd1c4997
Tony D Nguyen <tonydng123@gmail.com>
2023-09-21 11:49:26 -0700

    add variables class

---
+++ workspace_getting_started/java_basics/src/java_basics/Variables.java
@@ -0,0 +1,18 @@
+package java_basics;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 34;
+		
+		System.out.println(x);
+		
+		x = 234;
+		System.out.println(x * 4);
+		System.out.println(x);
+		String words = "This is a sentence";
+		System.out.println(words + " there are some more words");
+	}
+
+}
